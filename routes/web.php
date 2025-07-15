e18449ea542328cc1afea48633ecb7379b8f6240
Salsabeel Noor <salsabeelnoor@gmail.com>
2025-07-15 14:08:11 +0600

    laravel form ongoing

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,27 @@ use App\Models\Job;
 Route::get('/', function () {
     return view('home');
 });
+
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->cursorPaginate(3);
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
-Route::get('/jobs/{id}', function ($id) {
-    
-        $job = Job::find($id);
-    return view('job', ['job' => $job]);
+
+Route::get('/jobs/create', function() {
+    return view('jobs.create');
 });
+
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+    dd('Hello from jobs');
+});
+
 Route::get('/contact', function () {
     return view('contact');
 });
