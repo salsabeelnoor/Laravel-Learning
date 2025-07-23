74cd99670b09934f72598a8319bdda37dbd0e801
Salsabeel Noor <salsabeelnoor@gmail.com>
2025-07-23 13:00:35 +0600

    example project done

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> --}}
 </head>
 <body class="h-full">
 
